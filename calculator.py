286859817f1d2be32c59dddc902d7ef4d823d3b2
execratio <iskakov.aldiyar.09@gmail.com>
2026-03-26 10:22:37 +0500

    Добавлены базовые функции калькулятора (add, subtract)

---
+++ calculator.py
@@ -0,0 +1,12 @@
+# calculator.py
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    print("Простой калькулятор")
+    print("Сложение 5 + 3 =", add(5, 3))
+    print("Вычитание 10 - 4 =", subtract(10, 4))
